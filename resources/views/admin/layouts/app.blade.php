9261d6ea1771bc77fcb0592ee08eb96de03ed2d4
Irfan Saf <irfansaf7@gmail.com>
2023-05-02 16:00:39 +0700

    Fixing the Time

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -39,5 +39,23 @@
 </div>
 <!-- Script-->
 @stack('scripts')
+<<script>
+    function formatDate(dateString) {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+
+        // Pad single-digit hours and minutes with a leading zero
+        hours = hours.toString().padStart(2, '0');
+        minutes = minutes.toString().padStart(2, '0');
+
+        return `${day} ${month} ${year} at ${hours}:${minutes}`;
+    }
+</script>>
 </body>
 </html>
